ef4cb8e49e6b5d38e130f55e5970b0b6d5e08d24
Gideon Moore <gmoore016@gmail.com>
2020-05-25 22:51:06 -0700

    Added tracker functionality

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import numpy
+import random
 import networkx as nx
 
 # Number of patients present in each exchange at the start of the process
@@ -19,6 +20,46 @@ TIME_LEN = 50
 id_iterator = 0
 
 
+class Tracker:
+    def __init__(self):
+        # For tracking economy's performance
+        self.matches = []
+        self.expiries = []
+        self.sizes = []
+        self.ages = []
+        self.probs = []
+
+    def add_matches(self, matches):
+        self.matches.append(matches)
+
+    def add_expiries(self, expiries):
+        self.expiries.append(expiries)
+
+    def add_size(self, sizes):
+        self.sizes.append(sizes)
+
+    def add_age(self, age):
+        self.ages.append(age)
+
+    def add_prob(self, difficulty):
+        self.probs.append(difficulty)
+
+    def get_matches(self):
+        return self.matches
+
+    def get_expiries(self):
+        return self.expiries
+
+    def get_sizes(self):
+        return self.sizes
+
+    def get_ages(self):
+        return self.ages
+
+    def get_probs(self):
+        return self.probs
+
+
 def add_patients(exchange, num_patients):
     """
     Adds numpatients patients to exchange
@@ -34,7 +75,7 @@ def add_patients(exchange, num_patients):
 
         # Create node for new patient
         new_prob = numpy.random.uniform()
-        exchange.add_node(new_id, prob=new_prob)
+        exchange.add_node(new_id, prob=new_prob, age=0)
 
         # For each existing node, test whether they match
         # Test for each node up to the newest
@@ -54,35 +95,52 @@ def add_patients(exchange, num_patients):
     return exchange.nodes()
 
 
-def pass_time(exchange):
+def pass_time(exchange, expiry_rate, tracker):
     """
     Whenever time passes, some patients are lost
     """
     expirations = 0
+    matches = 0
+
+    node_ages = nx.get_node_attributes(exchange, "age")
+    node_probs = nx.get_node_attributes(exchange, "prob")
 
     for patient in list(exchange.nodes()):
-        if numpy.random.uniform() < DEPARTURE_CHANCE:
+        # Possible node will have been removed by neighbor
+        if patient not in exchange.nodes():
+            continue
+
+        if numpy.random.uniform() < expiry_rate:
+            # Node has become critical
+            # Choose a random neighbor
+            neighbors = list(exchange.neighbors(patient))
+
+            # If they have at least one feasible match, match them
+            if neighbors:
+                # Remove neighbor from graph after matching
+                match = random.choice(neighbors)
+                tracker.add_age(node_ages[match])
+                tracker.add_prob(node_probs[match])
+                exchange.remove_node(match)
+                matches = matches + 2
+
+            # If they have no neighbors, they expire
+            else:
+                expirations = expirations + 1
+
+            # Either way, remove the patient
+            tracker.add_age(node_ages[patient])
             exchange.remove_node(patient)
-            expirations = expirations + 1
 
-    return expirations
+    # Age all remaining patients
+    for patient in exchange.nodes():
+        new_age = node_ages[patient] + 1
+        nx.classes.function.set_node_attributes(exchange, new_age, "age")
 
-
-def match(exchange):
-    """
-    Given an exchange, finds the maximal matching and removes
-    all matched elements
-    """
-
-    # Finds the maximal matching of the exchange
-    max_match = nx.algorithms.matching.maximal_matching(exchange)
-
-    # Remove all matched patients
-    for pair in max_match:
-        for patient in pair:
-            exchange.remove_node(patient)
-
-    return 2 * len(max_match)
+    # Track data
+    tracker.add_expiries(expirations)
+    tracker.add_matches(matches)
+    tracker.add_size(exchange.order())
 
 
 def main():
@@ -90,36 +148,42 @@ def main():
     weekly = nx.Graph()
     monthly = nx.Graph()
 
-    print("Weekly matches: ")
-    expirations = 0
-    matches = 0
+    # Initiates trackers for stats
+    weekly_stats = Tracker()
+    monthly_stats = Tracker()
+
     add_patients(weekly, START_SIZE)
-    for i in range(TIME_LEN):
+    print("Weekly:")
+    weekly_sequence = []
+    for i in range(FREQ * TIME_LEN):
         # Add patients
         add_patients(weekly, INFLOW)
-        # Match patients if scheduled
-        matches = match(weekly) + matches
-        # Atrophy patients
-        expirations = pass_time(weekly) + expirations
-    print(matches)
-    print(expirations)
 
-    print("Monthly matches: ")
-    expirations = 0
-    matches = 0
+        # Patients become critical
+        pass_time(weekly, EX_RATE, weekly_stats)
+        weekly_sequence.append(weekly.order())
+
+    print("Matches: " + str(sum(weekly_stats.get_matches())))
+    print("Expiries: " + str(sum(weekly_stats.get_expiries())))
+    print("Pool Size: " + str(sum(weekly_stats.get_sizes())//len(weekly_stats.get_sizes())))
+    print("Average Age: " + str(sum(weekly_stats.get_ages())/len(weekly_stats.get_ages())))
+
+    print("Monthly:")
+    monthly_sequence = []
     add_patients(monthly, START_SIZE)
+    modified_expiry = 1 - (1 - EX_RATE) ** FREQ
     for i in range(TIME_LEN):
         # Add patients
-        add_patients(monthly, INFLOW)
-
-        # Match patients if scheduled
-        if not i % FREQ:
-            matches = match(monthly) + matches
+        add_patients(monthly, FREQ * INFLOW)
 
         # Atrophy patients
-        expirations = pass_time(monthly) + expirations
-    print(matches)
-    print(expirations)
+        pass_time(monthly, modified_expiry, monthly_stats)
+        monthly_sequence.append(monthly.order())
+
+    print("Matches: " + str(sum(monthly_stats.get_matches())))
+    print("Expiries: " + str(sum(monthly_stats.get_expiries())))
+    print("Pool Size: " + str(sum(monthly_stats.get_sizes())/len(monthly_stats.get_sizes())))
+    print("Average Age: " + str(FREQ * sum(weekly_stats.get_ages())/len(weekly_stats.get_ages())))
 
 
 main()
